bc6d690a47e2a5f245093362a27a695908b68375
Ira Herman <ira@xynetik.com>
2011-07-08 17:36:45 -0700

    Updated .gitignore with Michael Hartl's version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '13ab74b4107f5a99983de73d17eec7a621449f288e35f34f9f461e5a408e5033073745b2f1ef3013aa9d1eabcbea228bc3a726207430202a55bfbc748d009398'
