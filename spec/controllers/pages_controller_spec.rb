bb486e313c44719e973fec5c10cebb5097d521d2
Ira Herman <ira@xynetik.com>
2011-07-17 16:58:36 -0700

    Updated rspec tests using base_title variable

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
 render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App" 
+  end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -16,7 +20,7 @@ render_views
     it "should be have the right title" do
       get 'home'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => "#{@base_title} | Home")
     end
   end
 
@@ -29,7 +33,7 @@ render_views
     it "should be have the right title" do
       get 'contact'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
   end
 
@@ -37,7 +41,7 @@ render_views
     it "should be have the right title" do
       get 'about'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => "#{@base_title} | About")
     end
 
     it "should be successful" do
