06d6477c7728129600425325841163c7d8b04bde
Ira Herman <ira@xynetik.com>
2011-10-11 11:11:17 -0700

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,7 @@
 class User < ActiveRecord::Base
   
   attr_accessor :password
-  
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -22,5 +21,7 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex},
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true,
+                       :confirmation => true
 end
 
